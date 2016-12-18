9e6ca5a0beb2342dab495bb654674d27df5fda78
jillbore <mike.goin12@gmail.com>
2016-12-18 00:27:03 -0500

    Implemented basic Brainfuck-to-C interpreter

--- bfc.cpp
+++ bfc.cpp
@@ -2,10 +2,40 @@
 #include <fstream>
 
 int main(int argc, char** argv) {
+    char c;
 	std::ifstream infile;
+	std::ofstream outfile;
 
-	if (argc != 2)
-		printf("Invalid arguments - Expected: bfc filename\n");
+	if (argc != 3)
+		printf("Invalid arguments - Expected: bfc bf-file output-file\n");
 
+	infile.open(argv[1]);
+	outfile.open(argv[2]);
+
+    outfile << "int main() {\nchar mem[65536] = {0};\nint p = 0;\n";
+    while (infile.get(c)) {
+		switch(c) {
+            case '+':
+                outfile << "mem[p]++;\n"; break;
+            case '-':
+                outfile << "mem[p]--;\n"; break;
+            case '>':
+                outfile << "p++;\n"; break;
+            case '<':
+                outfile << "p--;\n"; break;
+            case '.':
+                outfile << "putchar(mem[p]);\n"; break;
+            case ',':
+                outfile << "mem[p] = getchar();\n"; break;
+            case '[':
+                outfile << "while (mem[p]) {\n"; break;
+            case ']':
+                outfile << "}\n"; break;
+        }
+    }
+    outfile << "return 0;\n}";
+
+    infile.close();
+    outfile.close();
 	return 0;
 }
