cb466e8fa4b114922938cdac33c007a6021cce8a
jillbore <mike.goin12@gmail.com>
2016-12-17 22:55:26 -0500

    Initial commit

---
+++ bfc.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <fstream>
+
+int main(int argc, char** argv) {
+	std::ifstream infile;
+
+	if (argc != 2)
+		printf("Invalid arguments - Expected: bfc filename\n");
+
+	return 0;
+}
